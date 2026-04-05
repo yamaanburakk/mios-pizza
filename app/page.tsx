3493b17638273eec087d5dfcb1e1eeddb3b0682e
BURAK YAMAN <burakyaman@192.168.1.124>
2026-04-05 20:10:23 +0300

    Fixes...

--- app/page.tsx
+++ app/page.tsx
@@ -182,9 +182,6 @@ export default function Home() {
           </motion.div>
         </div>
       </section>
-
-      <Gallery />
-
       {/* Call to Action */}
       <section className="py-16 bg-gradient-to-br from-gray-50 to-gray-100 relative overflow-hidden">
         <div className="container mx-auto px-4 text-center relative z-10">
